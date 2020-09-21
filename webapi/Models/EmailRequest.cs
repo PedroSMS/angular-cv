5e22cf318db83fd9922f8b520691d18652e0d198
PedroSMS <pedrosms@outlook.pt>
2020-09-21 14:03:39 +0100

    re-arrange structure folder

--- webapi/Models/EmailRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace webapi.Models
-{
-    public class EmailRequest
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Text { get; set; }
-    }
-}
