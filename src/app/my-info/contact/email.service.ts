08f1a432414b474ccf9ea148ffda0a3652417224
PedroSMS <pedrosms@outlook.pt>
2020-09-24 13:10:58 +0100

    fix consuming api

--- src/app/my-info/contact/email.service.ts
+++ src/app/my-info/contact/email.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { catchError } from 'rxjs/operators';
 import { EmailRequest } from 'src/app/models/email-request';
 
@@ -11,12 +12,13 @@ export class EmailService {
 
   constructor(private http: HttpClient) { }
 
-  send(emailRequest: EmailRequest): Observable<any>{
+  send(emailRequest: EmailRequest): Observable<string>{
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
     });
 
-    return this.http.post<any>('https://us-central1-nodejs-api-cv.cloudfunctions.net/webApi/email', emailRequest, { headers: headers} ).pipe(
+    return this.http.post('https://us-central1-nodejs-api-cv.cloudfunctions.net/webApi/email', emailRequest, { headers: headers, responseType: 'text'})
+    .pipe(
       catchError(err => {
         return of(null);
       })
